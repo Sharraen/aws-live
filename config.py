0667ad96fc9629e00cc839ddb6bbf319c8c6306f
Sharraen <99467263+Sharraen@users.noreply.github.com>
2025-12-01 13:06:34 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.ca10cskc4rm5.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "admin123"
+customdb = "employee"
+custombucket = "sharraeng-employee"
+customregion = "us-east-1"
+
