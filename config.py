be36e12a9192dfa8dbf61b205095d22315cc5cec
Sharraen <99467263+Sharraen@users.noreply.github.com>
2025-12-01 13:05:27 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-customhost = "employee.ca10cskc4rm5.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "admin123"
-customdb = "employee"
-custombucket = "sharraeng-employee"
-customregion = "us-east-1"
-
